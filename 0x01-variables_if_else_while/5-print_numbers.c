ee6d8b80d8eabf6c14910732997f6cd256183421
Adrian Orioki <adrianorioki@gmail.com>
2022-10-19 02:21:46 -0700

    Commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Assign a random number 
+ * Return: Always 0
+ */
+int main(void)
+{
+	int in;
+	
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+
+	return (0);
+}
