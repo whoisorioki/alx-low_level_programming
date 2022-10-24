1e2c38c63504f24655f17a2a7e4744ee18f6a775
Adrian Orioki <adrianorioki@gmail.com>
2022-10-24 01:00:28 -0700

    Commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	int i;
 
-	printf("Infiniteloop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
 	/**
-	 * while (i < 10)
+	 *while (i < 10)
 	 *{
 	 *	putchar(i);
 	 *}
