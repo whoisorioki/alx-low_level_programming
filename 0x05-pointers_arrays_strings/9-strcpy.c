94235f604779c2928387653ae4fd7973e18cc4c8
Adrian Orioki <adrianorioki@gmail.com>
2022-10-25 03:12:34 -0700

    Commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int len = 0;
 
-	while (*(src * len) != '\0')
+	while (*(src + len) != '\0')
 	{
 		*(dest + len) = *(src + len);
-		len++
+		len++;
 	}
 
 	*(dest + len) = '\0';
