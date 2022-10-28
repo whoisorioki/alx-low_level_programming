b51f44ad57f0e9a979abe894bd42d24b541f13db
Adrian Orioki <adrianorioki@gmail.com>
2022-10-28 02:51:03 -0700

    Commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,4 +5,16 @@
  * @n: pointer
  * Return: char
  */
-char *string_touppe
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
