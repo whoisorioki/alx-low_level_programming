3f2a60a6f396df91735d2567e7ff3f5640bf5562
Adrian Orioki <adrianorioki@gmail.com>
2022-11-01 18:35:26 -0700

    Commit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,9 +13,9 @@ void print_diagsums(int *a, int size)
 
 	for (b = O; b < size; b++)
 	{
-		sum1 += a[(size + 1) * b];
-		sum2 += a[(size - 1) * (b + 1)];
+		sum1 += *(a + (size * b + b));
+		sum2 += *(a + (size * b + size - 1 - b));
 	}
-
-	printf("%d, %d\n", sum1, sum2);
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
 }
