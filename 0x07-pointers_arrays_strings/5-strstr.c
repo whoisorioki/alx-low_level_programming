67f0e47b612946581c511d1c4bd337f85b7c1f03
Adrian Orioki <adrianorioki@gmail.com>
2022-11-01 18:11:39 -0700

    Commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurrence of the substring
+ * @haystack: entire string
+ * @needle: substring
+ * Return: pointer to the beginning of located substring, else NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h;
+	char *n;
+
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+
+		while (*haystack != '\0' && *n != '\0' && *haystack == *n)
+		{
+			haystack++;
+			n++;
+		}
+		if (!*n)
+			return (h);
+		haystack = h + 1;
+	}
+	return (0);
+}
