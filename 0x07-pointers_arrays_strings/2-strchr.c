0d1edc686c4458f39b8a7dcd7e1609736f94c4fb
Adrian Orioki <adrianorioki@gmail.com>
2022-11-01 17:27:32 -0700

    Commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,21 +2,19 @@
 
 /**
  * _strchr - locates a character in a string
- * @c: character
  * @s: string
- * Return: a pointer to the first occurrence, else NULL
+ * @c: character
+ * Return: the pointer to the first occurrence of the character c
  */
 char *_strchr(char *s, char c)
 {
-	while  (*s)
-	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
-	}
-	if (c == '\0')
-		return (s);
+	unsigned int i = 0;
 
-	return (NULL);
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
