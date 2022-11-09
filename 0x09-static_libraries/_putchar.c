2dd3019ae155b61c7890f71401b90f3946c6f8b8
Adrian Orioki <adrianorioki@gmail.com>
2022-11-09 13:35:34 -0800

    Commit

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,23 +1,10 @@
 #include <unistd.h>
-
-
-
 /**
- *
- *  * _putchar - writes the character c to stdout
- *
- *   * @c: The character to print
- *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately*/
-
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1, else -1
+ */
 int _putchar(char c)
-
 {
-
-		return (write(1, &c, 1));
-
+	return (write(1, &c, 1));
 }
