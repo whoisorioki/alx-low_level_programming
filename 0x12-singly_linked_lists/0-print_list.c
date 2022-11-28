161a19e9c1d4b3134eed90f71c9996a20a728f06
whoisorioki <adrianorioki@gmail.com>
2022-11-28 01:24:43 -0800

    Task O

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,9 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t el;
+	size_t elem;
 
-	el = 0;
+	elem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		el++;
+		elem++;
 	}
-	return (el);
+
+	return (elem);
 }
