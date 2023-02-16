c5d78169b4f3222fe48366fe3e4cf5f3557085fc
whoisorioki <adrianorioki@gmail.com>
2023-02-16 14:56:36 -0800

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",62);
+	return (1);
+}
