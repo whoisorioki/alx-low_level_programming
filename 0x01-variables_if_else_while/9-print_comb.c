fb18d3255c37bf04e8703427ee8c83e12809dde7
whoisorioki <adrianorioki@gmail.com>
2023-02-17 04:14:42 -0800

    Print combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 48;
+	while (a < 58)
+	{
+		if (a > 48)
+		{	
+			putchar(',');
+			putchar(' ');
+		}
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
