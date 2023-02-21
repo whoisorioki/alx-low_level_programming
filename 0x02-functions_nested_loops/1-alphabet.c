5c1cef2132b2e18a46e41ff969ce0dfa398182e9
whoisorioki <adrianorioki@gmail.com>
2023-02-21 00:23:30 -0800

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	a = 97;
+	while (a < 123)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+	return;
+}
