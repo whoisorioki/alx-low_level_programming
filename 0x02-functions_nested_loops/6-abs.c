3a60a688664ff2963e9283d6db55d309b90b2c21
whoisorioki <adrianorioki@gmail.com>
2023-02-21 02:13:34 -0800

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @int: integer
+ *
+ * Return: 0
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (-a);
+}
