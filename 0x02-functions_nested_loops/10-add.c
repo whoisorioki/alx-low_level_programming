fd2a16718a2181c71c187fb53584c9eab6829087
whoisorioki <adrianorioki@gmail.com>
2023-02-21 08:17:51 -0800

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
