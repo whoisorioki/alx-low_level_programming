34505c93e8dab08f726072f054b46e81fb39b133
whoisorioki <adrianorioki@gmail.com>
2023-02-22 01:33:02 -0800

    Largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,17 +13,23 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a > b && a > c)
-	{
 		largest = a;
-	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
-	else if (c > b && c > a)
-	{
+	else if (a > b && a < c)
 		largest = c;
-	}
+	else if (a < b && a > c)
+		largest = b;
+	else if (b > a && b > c)
+		largest = b;
+	else if (b > a && b < c)
+		largest = c;
+	else if (b < a && b > c)
+		largest = a;
+	else if (c > b && c > a)
+		largest = c;
+	else if (c > b && c < a)
+		largest = a;
+	else if (c < b && c > a)
+		largest = b;
 
 	return (largest);
 }
