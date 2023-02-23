5961f681774dca0a84623dc6a22bc5a955891cbf
whoisorioki <adrianorioki@gmail.com>
2023-02-23 04:10:06 -0800

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main- Entry point
+ * main - Entry point
  *
  * Return: 0
  */
@@ -21,4 +21,5 @@ int main(void)
 			printf("%d ", i);
 	}
 	printf("\n");
+	return (0);
 }
