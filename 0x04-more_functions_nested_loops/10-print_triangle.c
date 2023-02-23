e03c1c0aa55f99ba114ea1847389a934025f203a
whoisorioki <adrianorioki@gmail.com>
2023-02-23 04:58:57 -0800

    Print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle
+ * @size: size of triange
+ *
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i - 1; j++)
+				_putchar(32);
+			for (j = 0; j <= i; j++)
+				_putchar(35);
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
