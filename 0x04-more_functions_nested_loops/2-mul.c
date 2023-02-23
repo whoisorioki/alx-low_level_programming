ca9deaa4d4cb847569f6627a30ccaac5a7921f6f
whoisorioki <adrianorioki@gmail.com>
2023-02-23 02:22:05 -0800

    Multiply!!!

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
