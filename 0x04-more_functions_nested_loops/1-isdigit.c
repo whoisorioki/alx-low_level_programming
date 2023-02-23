d9f92d59a994b6e7612a8e42bce3a13eeac64f27
whoisorioki <adrianorioki@gmail.com>
2023-02-23 02:15:28 -0800

    Is it a digit?

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit 
+ * @c: character
+ *
+ * Return: 1 if digit, else 0
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+	else
+		return (0);
+}
