27f530bac35b739d1fc8fe77f31efc3ca1340d24
whoisorioki <adrianorioki@gmail.com>
2023-02-28 12:47:18 -0800

    Puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,12 +13,13 @@ void puts_half(char *str)
 	len = 0;
 	while (str[len] != '\0')
 		len++;
+	len--;
 	if (len % 2 == 0)
 		for (i = len / 2; str[i] != '\0'; i++)
 			_putchar(str[i]);
 	else
 		for (i = (len - 1) / 2; str[i] != '\0'; i++)
-			_putchar(str[i + 1]);
+			_putchar(str[i]);
 
 	_putchar('\n');
 }
