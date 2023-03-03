706a7379c74a074acb23085209a3860f6aeab542
whoisorioki <adrianorioki@gmail.com>
2023-03-03 04:24:47 -0800

    Strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: pointer to char, destination
+ * @char: pointer to char, source
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr;
+	int i;
+
+	i = 0;
+	ptr = dest;
+	while (*dest != '\0')
+		dest++;
+	while (*src != '\0' && i < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+	*dest = '\0';
+	return (ptr);
+}
