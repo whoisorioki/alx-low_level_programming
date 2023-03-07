256b1fabef889e4f63f64fc3865c485def302cb9
whoisorioki <adrianorioki@gmail.com>
2023-03-06 23:17:32 -0800

    Memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * memcpy - copies memory area
+ * _memcpy - copies memory area
  * @dest: destination string
  * @src: source string
  * @n: unsigned integer
@@ -13,8 +13,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while (src[i] != '\0')
-		src[i]++;
 	while (i < n)
 	{
 		dest[i] = src[i];
