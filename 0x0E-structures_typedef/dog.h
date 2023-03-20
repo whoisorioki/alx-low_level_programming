b251c877f00cc5bfbbc53d9c92076d831a9cbfd4
whoisorioki <adrianorioki@gmail.com>
2023-03-20 07:41:30 -0700

    Dog Woff!

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - Poppy
+ * @name: name of Dog
+ * @owner: owner of Dog
+ * @age: age  of Dog
+ */
 typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-}my_dog;
+} my_dog;
 
 #endif
