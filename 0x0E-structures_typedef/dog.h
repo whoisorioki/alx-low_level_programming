0219b46a1333f56f79f45a563aef5ce9a5fb3eb8
whoisorioki <adrianorioki@gmail.com>
2023-03-21 07:41:51 -0700

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,11 +6,17 @@
  * @owner: owner of Dog
  * @age: age  of Dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-} my_dog;
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
