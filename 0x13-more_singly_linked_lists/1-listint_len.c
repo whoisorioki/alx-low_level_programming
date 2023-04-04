e259173b31d67c5134864447918e21e7139a6974
whoisorioki <adrianorioki@gmail.com>
2023-04-04 13:28:37 -0700

    Listint_len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-        size_t node_count = 0;
-        
-        while (h != NULL)
-        {
-            node_count++;
-            h = h->next;
-        }
-        
-        return (node_count);
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		node_count++;
+		h = h->next;
+	}
+
+	return (node_count);
 }
